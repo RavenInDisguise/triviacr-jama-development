c7035b283eab58c334eea945c2575edeac25cd99
Mónica Alfaro Parrales <64928283+RavenInDisguise@users.noreply.github.com>
2021-06-04 01:04:50 -0600

    Actualización preguntas 2

--- ProyectoI.asm
+++ ProyectoI.asm
@@ -20,6 +20,9 @@ msj_prompt3  db 'Desea salir? ',0
 msj_prompt4  db 'Cual opcion desea realizar: ',0
 msj_prompt5  db 'Ranking: ',0
 msj_prompt6  db 'Su respuesta: ',0
+msj_prompt7  db 'Su respuesta fue correcta!',0AH,0
+msj_prompt8  db 'Su respuesta fue incorrecta!',0AH,0
+
 ;Preguntas
 question0  db 'q0',0AH,0
 question1  db 'Cual es el canton en el que se encuentra el volcan Poas? a) Poas b) Alajuela c) Perez Zeledon d) Sabanilla',0AH,0
@@ -27,44 +30,44 @@ question2  db 'A que canton pertenece el distrito de Vargas Araya? a) Montes de 
 question3  db 'Desamparados es el canton mas grande de Costa Rica: verdadero o falso',0AH,0
 question4  db 'El distrito de Sabanilla pertenece al canton de Montes de Oca: verdadero o falso',0AH,0
 question5  db 'En cual billete aparece Carmen Lyra? a)20 mil b)10 mil c)5 mil d)50 mil',0AH,0
-question6  db 'q6',0
-question7  db 'q7',0
-question8  db 'q8',0
-question9  db 'q9',0
-question10  db 'q10',0
+question6  db 'q6',0AH,0
+question7  db 'q7',0AH,0
+question8  db 'q8',0AH,0
+question9  db 'q9',0AH,0
+question10 db 'q10',0AH,0
 
 
 ;Respuestas
-array_resp      dd 'a','a','f','v','b'
+array_resp   db 'a','a','f','v','b','a','b','v','f','d','v'
 
 ;Puntos por pregunta
-array_pts_abc   dd 2,3,1,1
+array_pts   db 2,3,1,1
 
 ;Preguntas ya respondidas
-array_done_users    TIMES 30  dd '~'
+array_done_quest    TIMES 30  dd -1
 
 ;Usuarios jugando
-array_ingame_users  TIMES 5  dd '~'
-array_users_pts     TIMES 5  dd -1
+array_ingame_users  TIMES 5   dd '~'
+array_users_pts     TIMES 5   dd -1
 
 ;Usuarios en ranking
 array_users         TIMES 11  dd '~'
 
 .UDATA
-players         resd 1              
+players     resd 1              
 ;Cantidad de jugadores ingame
-nickname        resb 1              
+nickname    resb 1              
 ;Nicknames de cada usuario
-result          resb 1  
-answer          resb 1
-option          resd 1
+result      resb 1  
+answer      resb 1
+option      resd 1
+value       resd 5
 
 .CODE
 
 ;******************************************* 
 ;MACRO:
 ;******************************************* 
-
 %macro five_questions 0
     cmp EAX,0
     je  %%0
@@ -177,15 +180,36 @@ get_random_number:
     sub     EDX, EDX
     mov     ECX, 10-0+1         ;Numeros random de 0 a 30
     div     ECX
-    mov     EAX,EDX             ;PutLInt EAX
+    mov     EAX,EDX             
+    ;PutLInt EAX
     nwln
-    call question_selection
+get_answered_questions:
+    cmp     dword[array_done_quest+EBX*4],EAX
+    je      get_random_number
+    PutLInt dword[array_done_quest+EBX*4]
+    cmp     dword[array_done_quest+EBX*4],-1
+    jne     get_answered_questions
+    inc     EBX
+    cmp     EBX,31
+    je      ranking
+    call    question_selection
 get_answer_user:
     PutStr  msj_prompt6
-    GetStr  answer
-    mov     EDX,answer
-    cmp     EDX,byte[array_resp+EAX]
-    jne     get_answer_user
+    GetCh   [answer]
+    mov     DL, byte[answer]
+    cmp     byte[array_resp+EAX],DL
+    je      is_correct
+    PutStr  msj_prompt8
+    jmp     get_random_number
+is_correct:
+    sub     EDX,EDX
+    PutStr  msj_prompt7
+    mov     dword[array_done_quest+EDI*4],EAX
+    mov     EDX,dword[array_done_quest+EDI*4]
+    inc     EDI
+    PutLInt EDX
+    ;PutLInt EDI
+    jmp     get_random_number
 done:
     .EXIT
 
@@ -193,12 +217,12 @@ done:
 ;PROCEDURE:
 ;*******************************************    
 question_selection:
-    cmp AL,5
+    cmp EAX,5
     jg question_selection2
     five_questions  
     jmp return 
 question_selection2:
-    cmp AL,10
+    cmp EAX,10
     jg question_selection3
     ten_questions
     jmp return 
